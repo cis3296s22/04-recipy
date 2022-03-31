8402a4e0f89c4147cd1e42e1fbda5e2a2cf91cbb
Nate Symer <nate@symer.io>
2022-03-31 17:52:51 -0400

    Wrote scaffolding for react app

--- recipy/webpack/jsx/recipe.jsx
+++ recipy/webpack/jsx/recipe.jsx
@@ -1,2 +1,10 @@
-console.log(window._json);
-debugger;
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+let recipe = JSON.parse(window._json);
+
+const Recipe = ({recipe}) => {
+    return <div>hello, {recipe.owner.first_name} {recipe.owner.last_name}</div>;
+};
+
+ReactDOM.render(<Recipe recipe={recipe} />, document.querySelector('#content'));

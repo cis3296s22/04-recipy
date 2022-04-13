1a98fbffdb7a898dedb29d32d1b56a6fb1dd2134
Russell Abernethy <rdaswimmer@gmail.com>
2022-04-12 22:43:45 -0400

    wrote tests

--- recipy/tests.py
+++ recipy/tests.py
@@ -1,1 +1,21 @@
-from django.test import TestCase
+from django.test import Client
+
+import unittest
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_recipy(self):
+
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/recipe/1')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/accounts/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('user')
+        self.assertEqual(response.status_code, 200)
